51ea3688dd40547865e27850860f75cb03189837
Lip Cowan <pcowan90@gmail.com>
2020-07-22 08:24:33 -0700

    added new folder stateDrills

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+Class HelloClass extends React.Component {
+    render() {
+        return (
+            <div className="helloWorld">Hello World!</div>
+        )
+    };
+};
+
+
+
+
+
+
+export default Hello;
