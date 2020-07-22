2b52713b44c14644b6af28cb31ec88b4f35eec42
Lip Cowan <pcowan90@gmail.com>
2020-07-22 08:52:39 -0700

    finished 1st drill

--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
 
-Class HelloClass extends React.Component {
-    render() {
-        return (
-            <div className="helloWorld">Hello World!</div>
-        )
-    };
-};
+class Hello extends React.Component {
+  state = {
+      who: ''
+  };
 
+handleButtonClick = (btnString) => {
+    this.setState({who: btnString})
+}
 
-
-
-
+  
+  render() {
+    return (
+      <div>
+        <p>Hello, {this.state.who}</p>
+        <button className="world" onClick={() => this.handleButtonClick("World!")} > World</button>
+        <button className="react" onClick={() => this.handleButtonClick("React!")}>React</button>
+        <button className="friend" onClick={() => this.handleButtonClick("Friend!")}>Friend</button>
+        <button className="empty" onClick={() => this.handleButtonClick('')}>Clear</button>
+      </div>
+    );
+  }
+}
 
 export default Hello;
