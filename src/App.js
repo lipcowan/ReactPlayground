4560160539c21fb48ffa73155147d572ceaaaacd
Lip Cowan <pcowan90@gmail.com>
2020-07-22 08:07:02 -0700

    prepare for remote

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Split from "./composition/Split";
 import "./App.css";
+import Tooltip from "./composition/Tooltip"
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
+
 
 function App() {
   return (
     <main className="App">
       <Split className="left" flexBasis={4}>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet
+        This is the content for the left `Split`. Lorem {firstTooltip} ipsum dolor sit amet
         consectetur, adipisicing elit. Incidunt ex velit suscipit facere
         officia?
       </Split>
+      <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
       <Split className="right">
         This is the content for the right `Split`. Inventore aliquid cupiditate
-        suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus
+  suscipit repellat. Quaerat quis {secondTooltip} officiis quam fuga. Aliquid quo possimus
         id soluta aspernatur.
       </Split>
     </main>
