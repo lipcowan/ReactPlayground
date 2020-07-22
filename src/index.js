ac77ef1389343c3a8df62b1ae1738a9fae24a78f
michaelcodes1993 <lpskirito7@outlook.com>
2020-07-22 15:16:33 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 //import App from './App';
-import './index.css';
-import Hello from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
+import "./index.css"
+//import Hello from './state-drills/HelloWorld';
+import Bomb from "./state-drills/Bomb"
 
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById("root"))
--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,38 +1,40 @@
-import React from 'react'
+import React from "react"
 
 class Bomb extends React.Component {
+	constructor(props) {
+		super(props)
+		this.interval = null
+	}
 
- constructor(props){
-     super(props);
-     this.interval = null
-     
-    }
-     
-    componentDidMount(){
-        this.interval = window.setInterval(() => this.setState({count:this.state.count +1}), 1000);
-    }
- componentWillUnmount(){
-     window.clearInterval(this.interval);
- }
-     
-   state={
-         count: 0
-     }
-   
-//scope.clearInterval(intervalID)
- // when the component mounts +1 when it unmounts clear it when divisible by 2 tick otherwise tock if 8 or above boom
-     render() {
-         let bomb = "";
-         if(this.state.count >= 8){
-            bomb= 'BOOM!!!!'
-         }else if(this.state.count % 2 === 0) {
-             bomb = 'Tick'
-         }else bomb = 'Tock';
-         return (
-         <div className="tick-tock-boom"><p>{bomb}</p></div>
-         )
-     }
+	componentDidMount() {
+		this.interval = window.setInterval(
+			() => this.setState({ count: this.state.count + 1 }),
+			1000
+		)
+	}
+	componentWillUnmount() {
+		window.clearInterval(this.interval)
+	}
+
+	state = {
+		count: 0,
+	}
+
+	//scope.clearInterval(intervalID)
+	// when the component mounts +1 when it unmounts clear it when divisible by 2 tick otherwise tock if 8 or above boom
+	render() {
+		let bomb = ""
+		if (this.state.count >= 8) {
+			bomb = "BOOM!!!!"
+		} else if (this.state.count % 2 === 0) {
+			bomb = "Tick"
+		} else bomb = "Tock"
+		return (
+			<div className="tick-tock-boom">
+				<p>{bomb}</p>
+			</div>
+		)
+	}
 }
 
-
-export default Bomb;
+export default Bomb
